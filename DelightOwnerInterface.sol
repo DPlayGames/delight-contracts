bb980c8b8f176668b847ed88fbd444dae8fc1d9d
codingcomposer <43369346+codingcomposer@users.noreply.github.com>
2019-09-19 14:13:00 +0900

    Update DelightOwnerInterface.sol

--- DelightOwnerInterface.sol
+++ DelightOwnerInterface.sol
@@ -2,15 +2,19 @@ pragma solidity ^0.5.9;
 
 interface DelightOwnerInterface {
 	
+	// Gets the record IDs of the owner.
 	// 소유주의 기록 ID들을 가져옵니다.
 	function getRecordIds(address owner) view external returns (uint[] memory);
 	
+	// Gets the building IDs of the owner.
 	// 소유주의 건물 ID들을 가져옵니다.
 	function getBuildingIds(address owner) view external returns (uint[] memory);
 	
+	// Gets the army IDs of the owner.
 	// 소유주의 부대 ID들을 가져옵니다.
 	function getArmyIds(address owner) view external returns (uint[] memory);
 	
+	// Gets the knight IDs of the owner.
 	// 소유주의 기사 ID들을 가져옵니다.
 	function getKnightItemIds(address owner) view external returns (uint[] memory);
 }
