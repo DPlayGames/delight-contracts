b968d3bf4f8084e26bbeba001ed58af8a5f979ff
codingcomposer <43369346+codingcomposer@users.noreply.github.com>
2019-09-19 14:09:52 +0900

    Update DelightKnightItemInterface.sol

--- DelightKnightItemInterface.sol
+++ DelightKnightItemInterface.sol
@@ -16,9 +16,11 @@ interface DelightKnightItemInterface {
     function getApproved(uint _tokenId) external view returns (address);
     function isApprovedForAll(address _owner, address _operator) external view returns (bool);
     
+    // Returns the number of items.
     // 아이템의 개수를 반환합니다.
 	function getItemCount() external view returns (uint);
 	
+	// Returns the number of items.
 	// 아이템의 정보를 반환합니다.
 	function getItemInfo(uint itemId) external view returns (
 		uint hp,
@@ -27,15 +29,19 @@ interface DelightKnightItemInterface {
 		uint buffDamage
 	);
 	
+	// Returns the HP given to the knight of the item.
 	// 아이템의 기사에게 부여하는 HP를 반환합니다.
 	function getItemHP(uint itemId) external view returns (uint);
 	
+	// Returns the damage given to the knight of the item.
 	// 아이템의 기사에게 부여하는 데미지를 반환합니다.
 	function getItemDamage(uint itemId) external view returns (uint);
 	
+	// Returns the buff HP of the item.
 	// 아이템의 버프 HP를 반환합니다.
 	function getItemBuffHP(uint itemId) external view returns (uint);
 	
+	// Returns the buff damage of the item.
 	// 아이템의 버프 데미지를 반환합니다.
 	function getItemBuffDamage(uint itemId) external view returns (uint);
 }
